53b3dea93287f45f047ba918251bfaeda0f8275c
MatNimv <matilda.nimvik@gmail.com>
2021-09-20 17:03:41 +0200

    update eyyyy

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 "use strict";
 
-const mainUser = 12;
+const mainUser = { 
+    id: 12, 
+    alias: "MatildaN", 
+    favs: []
+    };
 
+let paintingObj = [];
 //få ut alla users från SameTaste DB
 async function getSameTasteUsers(){
     const response = await fetch("http://mpp.erikpineiro.se/dbp/sameTaste/users.php");
     const data = await response.json();
-    console.log(data);
     data.message.sort((a,b) => a.alias > b.alias)
 
     data.message.forEach(element => {
@@ -34,7 +38,6 @@ getPaintingsIDs()
 
 async function getPaintingInfo(arrayOfIDs){
     const arrayOfFetchPromises = [];
-    const objectsArray = [];
 
     //går genom varje ID och får tillbaka en lista av promises
     arrayOfIDs.objectIDs.forEach(oneID => {
@@ -53,92 +56,70 @@ async function getPaintingInfo(arrayOfIDs){
     const arrayOfJSONData = await Promise.all(arrayOfJSONPromises);
 
     //sorterar bort onödiga nycklar från datan och skapar ny array av målningsobjekten
-    let paintingObj = arrayOfJSONData.map(pain => { return {
+    paintingArr = arrayOfJSONData.map(pain => { return {
         objectID: pain.objectID,
         primaryImageSmall: pain.primaryImageSmall,
         title: pain.title,
         artistDisplayName: pain.artistDisplayName
     }});
-    paintingObj.sort((a,b) => a.artistDisplayName > b.artistDisplayName);
+    paintingArr.sort((a,b) => a.artistDisplayName > b.artistDisplayName);
 
-    //varje painting object sätts in till en array
-    //arrayen blir ett returnvärde för nästa funktion
-    objectsArray.push(paintingObj);
+    //gör en string av hela arrayen , som sätts in i en localStorage
+    let stringyObj = JSON.stringify(paintingArr);
+    localStorage.setItem(`Paintings`,`${stringyObj}`);
+
+
+    //localStorage.setItem(push(paintingObj));
     //varje målning sätts in i localStorage.
-    paintingObj.forEach(element => {
-        localStorage.setItem(`${element.title}`, `${element.objectID}`);
-    })
-
     //paintingObj.forEach(element => {
-    //    let paintingDiv = document.createElement("div");
-    //    let listOfPaintings = document.getElementById("listOfPaintings");
-//
-    //    paintingDiv.innerHTML = `
-    //    <div class="onePainting">
-    //    <button class="add">ADD</button>
-    //        <img src="${element.primaryImageSmall}">
-    //        <div class="paintingText">
-    //            <p>${element.artistDisplayName}</p>
-    //            <p>${element.title}</p>
-    //        </div>
-    //    </div>
-    //    `;
-    //    listOfPaintings.append(paintingDiv);
-    //});
-    console.log(objectsArray);
-    return objectsArray;
-}
+    //    localStorage.setItem(`${element.objectID}`, `${element.title}`);
+    //})
 
-function showPaintings(paintingObjsArray){
-    
-    console.log(getPaintingInfo(paintingObjsArray));
-    getPaintingInfo(paintingObjsArray).forEach(element => {
+    paintingObj.forEach(element => {
         let paintingDiv = document.createElement("div");
         let listOfPaintings = document.getElementById("listOfPaintings");
+        paintingDiv.classList.add("onePainting")
 
         paintingDiv.innerHTML = `
-        <div class="onePainting">
             <img src="${element.primaryImageSmall}">
             <div class="paintingText">
                 <p>${element.artistDisplayName}</p>
                 <p>${element.title}</p>
             </div>
-        </div>
         `;
+        paintingDiv.prepend(yourHandler());
         listOfPaintings.append(paintingDiv);
     });
+    console.log(paintingObj);
+    return paintingObj;
 }
-showPaintings();
-//showPaintings(getPaintingInfo)
 
-//addera en målning till favoritlistan. uH
-document.querySelector("#listOfPaintings").addEventListener("click", function(){
-    //koppla ihop .add knappens målning (titel på målning) med dess objectID
-    //som sedan sätts in i addFav.
-    let findTitle = 
-    fetch(new Request("http://mpp.erikpineiro.se/dbp/sameTaste/users.php",
-    {
+//såhär ska jag skriva för att få ut alla objekt från localStorage
+    let localParse = JSON.parse(localStorage.getItem("Paintings"));
+    //specifikt detta i konsolloggen
+    console.log(localParse);
+
+function yourHandler(choice){
+    let button = document.createElement("button");
+
+    button.innerHTML = "ADD";
+    button.classList.add("add");
+
+    if (button.classList.contains("add")){
+        button.addEventListener("click", function (e){
+            
+            let click = e.target.nextElementSibling.nextElementSibling.innerHTML;
+            console.log(click);
+            console.log(e.target);
+
+        let request = new Request('http://mpp.erikpineiro.se/dbp/sameTaste/users.php');
+        fetch(request), 
+        {
         method: "PATCH",
-        body: JSON.stringify({id: mainUser, addFav: objectID}),
-        headers:  {"Content-type": "application/json; charset=UTF-8"}, 
-    }))
-    .then( response =>{
-        if (response.status == 409){
-            console.log("no more favs");
+        body: JSON.stringify({id: mainUser.id, addFav: []}),
+        headers: {"Content-type": "application/json; charset=UTF-8"}, 
         }
-        else if (response.status == 404){
-            console.log("finns ej.");
-        }else if (response.status == 400){
-            console.log("nah");
-        }else if (response.status == 415){
-            console.log("skicka en JSON TACK.");
-        }else {
-            return response.json();
-        }
-    })
-});
-
-
-function deleteFav(){
-
+        })
+    }
+    return button;
 }
--- skräp.js
+++ skräp.js
@@ -14,3 +14,135 @@
 
     //inte mer än 2 specifika scrollbars på sidan. Men på punkt 10 grafiken
     //säger du att om viewern är mindre = scrollbars. EH??
+
+
+    //genom datan från getPaintingInfo, sätts det in i showPaintings.
+//getPaintingInfo()
+//    .then(data => showPaintings(data))
+
+
+//function showPaintings(paintingObjsArray){
+//    
+//    console.log(getPaintingInfo(paintingObjsArray));
+//    getPaintingInfo(paintingObjsArray).forEach(element => {
+//        let paintingDiv = document.createElement("div");
+//        let listOfPaintings = document.getElementById("listOfPaintings");
+//
+//        paintingDiv.innerHTML = `
+//        <div class="onePainting">
+//            <img src="${element.primaryImageSmall}">
+//            <div class="paintingText">
+//                <p>${element.artistDisplayName}</p>
+//                <p>${element.title}</p>
+//            </div>
+//        </div>
+//        `;
+//        listOfPaintings.append(paintingDiv);
+//    });
+//}
+
+//addera en målning till favoritlistan. uH
+//document.querySelector(".add").addEventListener("click", function(){
+//    //koppla ihop .add knappens målning (titel på målning) med dess objectID
+//    //som sedan sätts in i addFav.
+//    let findTitle = 
+//    fetch(new Request("http://mpp.erikpineiro.se/dbp/sameTaste/users.php",
+//    {
+//        method: "PATCH",
+//        body: JSON.stringify({id: mainUser, addFav: objectID}),
+//        headers:  {"Content-type": "application/json; charset=UTF-8"}, 
+//    }))
+//    .then( response =>{
+//        if (response.status == 409){
+//            console.log("no more favs");
+//        }
+//        else if (response.status == 404){
+//            console.log("finns ej.");
+//        }else if (response.status == 400){
+//            console.log("nah");
+//        }else if (response.status == 415){
+//            console.log("skicka en JSON TACK.");
+//        }else {
+//            return response.json();
+//        }
+//    })
+//});
+
+//jag vet inte hur jag ska hantera flera klasser med samma klassnamn
+//so yah
+//if (document.body.addEventListener){
+//    document.body.addEventListener('click',yourHandler,false);
+//}
+//else{
+//    document.body.attachEvent('onclick',yourHandler);
+//}
+//
+//function yourHandler(e){
+//    e = e || window.event;
+//    var target = e.target || e.srcElement;
+//    if (target.className.match("add"))
+//    {
+//    //koppla ihop .add knappens målning (titel på målning) med dess objectID
+//    //som sedan sätts in i addFav.
+//
+//    //let findImageID = paintingObj.find(painting => {
+//    //    target.nextSibling.nextSibling.lastChild.innerHTML == painting.title;
+//    //});
+//
+//    let click = target.nextSibling.nextSibling.innerHTML;
+//    console.log(click);
+//    fetch(new Request("http://mpp.erikpineiro.se/dbp/sameTaste/users.php",
+//    {
+//        method: "PATCH",
+//        body: JSON.stringify({id: mainUser.id, addFav: objectID}),
+//        headers:  {"Content-type": "application/json; charset=UTF-8"}, 
+//    }))
+//    .then( response =>{
+//        if (response.status == 409){
+//            console.log("no more favs");
+//        }
+//        else if (response.status == 404){
+//            console.log("finns ej.");
+//        }else if (response.status == 400){
+//            console.log("nah");
+//        }else if (response.status == 415){
+//            console.log("skicka en JSON TACK.");
+//        }else {
+//            return response.json();
+//
+//        }console.log("hej");//an element with the keyword Class was clicked
+//    })
+//}}
+
+//function findButton(e){
+//
+//    if(e.target.classList.includes("add")){
+////addera en målning till favoritlistan. uH
+//document.querySelector(".add").addEventListener("click", function(){
+//    //koppla ihop .add knappens målning (titel på målning) med dess objectID
+//    //som sedan sätts in i addFav.
+//    let findTitle = 
+//    fetch(new Request("http://mpp.erikpineiro.se/dbp/sameTaste/users.php",
+//    {
+//        method: "PATCH",
+//        body: JSON.stringify({id: mainUser, addFav: objectID}),
+//        headers:  {"Content-type": "application/json; charset=UTF-8"}, 
+//    }))
+//    .then( response =>{
+//        if (response.status == 409){
+//            console.log("no more favs");
+//        }
+//        else if (response.status == 404){
+//            console.log("finns ej.");
+//        }else if (response.status == 400){
+//            console.log("nah");
+//        }else if (response.status == 415){
+//            console.log("skicka en JSON TACK.");
+//        }else {
+//            return response.json();
+//        }
+//        console.log("hej");
+//    })
+//});
+//}
+//}
